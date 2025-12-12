40aeb81ec2024231d6d3fab3647548266dca2170
KevinElevenn <128264558+KevinElevenn@users.noreply.github.com>
2025-12-11 21:14:31 -0800

    disabled Event CS
    disabled Event CS

--- Assets/Scripts/Events/Event.cs
+++ Assets/Scripts/Events/Event.cs
@@ -1,55 +1,55 @@
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.InputSystem;
-
-public abstract class Event : MonoBehaviour
-{
-    [SerializeField] private List<Waypoint> _waypoints = new List<Waypoint>();
-
-    public UnityEvent OnEventStarted;
-    public UnityEvent OnEventFinish;
-    public UnityEvent<string> OnUpdateObjective;
-
-    private List<KeyObjects> _currentKeys = new List<KeyObjects>();
-    public int _currentKeysCount => _currentKeys.Count;
-
-    void OnValidate()
-    {
-        FindwayPoints();
-    }
-
-    private void FindwayPoints()
-    {
-        _waypoints = GetComponentsInChildren<Waypoint>().ToList();
-    }
-
-    public virtual void StartEvent()
-    {
-        OnEventStarted.Invoke();
-    }
-
-    public virtual void StartFinish()
-    {
-        OnEventFinish.Invoke();
-    }
-
-    protected void UpdateObjective(string message)
-    {
-        OnUpdateObjective.Invoke(message);
-    }
-
-    protected Transform GetRandomSpawnPoint()
-    {
-        int randomIndex = Random.Range(0, _waypoints.Count);
-        return _waypoints[randomIndex].transform;
-    }
-
-    protected void SpawnKey(KeyObjects objects)
-    {
-        Transform spawnPoint = GetRandomSpawnPoint();
-        KeyObjects spawned = Instantiate(objects, spawnPoint.position, spawnPoint.rotation, transform);
-        _currentKeys.Add(spawned);
-    }
-}
+// using System.Collections.Generic;
+// using System.Linq;
+// using UnityEngine;
+// using UnityEngine.Events;
+// using UnityEngine.InputSystem;
+// 
+// public abstract class Event : MonoBehaviour
+// {
+//     [SerializeField] private List<Waypoint> _waypoints = new List<Waypoint>();
+// 
+//     public UnityEvent OnEventStarted;
+//     public UnityEvent OnEventFinish;
+//     public UnityEvent<string> OnUpdateObjective;
+// 
+//     private List<KeyObjects> _currentKeys = new List<KeyObjects>();
+//     public int _currentKeysCount => _currentKeys.Count;
+// 
+//     void OnValidate()
+//     {
+//         FindwayPoints();
+//     }
+// 
+//     private void FindwayPoints()
+//     {
+//         _waypoints = GetComponentsInChildren<Waypoint>().ToList();
+//     }
+// 
+//     public virtual void StartEvent()
+//     {
+//         OnEventStarted.Invoke();
+//     }
+// 
+//     public virtual void StartFinish()
+//     {
+//         OnEventFinish.Invoke();
+//     }
+// 
+//     protected void UpdateObjective(string message)
+//     {
+//         OnUpdateObjective.Invoke(message);
+//     }
+// 
+//     protected Transform GetRandomSpawnPoint()
+//     {
+//         int randomIndex = Random.Range(0, _waypoints.Count);
+//         return _waypoints[randomIndex].transform;
+//     }
+// 
+//     protected void SpawnKey(KeyObjects objects)
+//     {
+//         Transform spawnPoint = GetRandomSpawnPoint();
+//         KeyObjects spawned = Instantiate(objects, spawnPoint.position, spawnPoint.rotation, transform);
+//         _currentKeys.Add(spawned);
+//     }
+// }
